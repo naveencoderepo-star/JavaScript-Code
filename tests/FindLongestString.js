ac52d9352c44aa6f1abc37b176b677aacef2f25a
naveencoderepo-star <naveencoderepo@gmail.com>
2026-01-29 16:18:31 +0530

    Code - Find longest word in String

---
+++ tests/FindLongestString.js
@@ -0,0 +1,23 @@
+class FindLongestWordInString {
+
+  findLongestWord(sentence) {
+    const words = sentence.split(" ");
+    let longestWord = "";
+
+    for (let word of words) {
+      if (word.length > longestWord.length) {
+        longestWord = word;
+      }
+    }
+
+    return longestWord;
+  }
+}
+
+// Create object of the class
+const obj = new FindLongestWordInString();
+
+// Call the method
+const result = obj.findLongestWord("Playwright  is sfksflskfsjfslfjslfsl");
+
+console.log(result);
