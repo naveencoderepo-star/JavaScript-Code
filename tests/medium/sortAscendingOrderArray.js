1beca3ceb13eb79d4878e04123e584885215549a
naveencoderepo-star <naveencoderepo@gmail.com>
2026-02-03 11:19:17 +0530

    Update sortAscendingOrderArray.js

--- tests/medium/sortAscendingOrderArray.js
+++ tests/medium/sortAscendingOrderArray.js
@@ -5,9 +5,19 @@ class SortArrayInAscendingOrder {
     for (let i = 0; i < lengthOfArray ; i++) {
       for (let j = i + 1; j < lengthOfArray; j++) {
         if (arr[i] > arr[j]) {
+
+          // Destructuring or Using Temp variable we can swap the numbers,
+
+          
           // Destructuring allows extracting values from arrays or objects into variables,
           // and it’s commonly used to swap values without a temporary variable
-          ;[arr[i], arr[j]] = [arr[j], arr[i]]
+          // ;[arr[i], arr[j]] = [arr[j], arr[i]]
+
+          // also we can do the swap by using the temp variable also,
+
+          let temp = arr[i]
+          arr[i]  = arr[j]
+          arr[j]  = temp;
         }
       }
     }
